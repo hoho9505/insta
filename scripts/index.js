dca5d0291717aadb677c4b142db2d8c75666d93c
Hyung_Junny <>
2020-05-09 17:33:19 +0900

    second assignment2

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,34 @@
 const dots = document.querySelector(".dots");
+const modal = document.querySelector(".modal");
+const modalList = document.querySelector(".modal_tags");
 
 function handleClick(){
-  const modal = document.querySelector(".modal");
-  console.log(modal);
   if(modal.classList.contains("hide"))
   {
     modal.classList.remove("hide");
   }
+  else
+  {
+    modal.classList.add("hide");
+  }
+}
+
+function handleOutsideClick(event)
+{
+  if(event.target !== dots)
+  {
+    let flag =0;
+    for(let i=0; i<6; i++)
+    {
+      if(event.target===modalList[i])
+      {
+        flag=1;
+        break;
+      }
+    }
+    if(!flag) modal.classList.add("hide");
+  }
 }
 
 dots.addEventListener("click", handleClick);
+window.addEventListener("click", handleOutsideClick);
